1403e4db10309aa89e5b9bd5c29ed08d7a126c7e
roufsyed <afrid704@gmail.com>
2026-05-29 15:31:42 +0530

    Bump version to 1.2 (versionCode 3)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.slate.launcher"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
     }
 
     signingConfigs {
