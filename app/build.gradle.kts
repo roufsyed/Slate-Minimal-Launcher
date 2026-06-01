11735e0a3e0fbda6b630279db0755c4e29e5a2ba
roufsyed <afrid704@gmail.com>
2026-06-01 21:34:05 +0530

    Migrate Kotlin jvmTarget to the compilerOptions DSL
    kotlinOptions { jvmTarget = "..." } became a hard error in
    Kotlin 2.3.20. Move the JVM-1.8 target to the new top-level
    kotlin { compilerOptions { jvmTarget = JvmTarget.JVM_1_8 } } block
    per kotl.in/u1r8ln.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,5 @@
 import java.util.Properties
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.android.application)
@@ -63,9 +64,11 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+}
 
-    kotlinOptions {
-        jvmTarget = "1.8"
+kotlin {
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_1_8
     }
 }
 
