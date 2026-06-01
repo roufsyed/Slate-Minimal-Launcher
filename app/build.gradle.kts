ebaf203d561aa8311b13f1c47c26b58bd093a394
roufsyed <afrid704@gmail.com>
2026-06-01 23:12:00 +0530

    Bump version to 1.3 (versionCode 4)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.slate.launcher"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 4
+        versionName = "1.3"
     }
 
     signingConfigs {
